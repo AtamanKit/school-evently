7c1fb2aec267d86a430e9c116bea0bfa5fa1ce73
AtamanKit <rodionatamaniuc@gmail.com>
2024-01-06 19:14:23 +0200

    added database mongoose

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,20 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-    publicRoutes: [
-        '/',
-        'events/:id',
-        '/api/webhook/clerk',
-        '/api/webhook/stipe',
-        'api/uploading'
-    ]
+  publicRoutes: [
+    '/',
+    'events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stipe',
+    'api/uploading'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stipe',
+    'api/uploading'
+  ]
 });
- 
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
