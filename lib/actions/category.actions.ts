851383ce5b066e6faba2cc251a47baf7e1f8b03c
AtamanKit <rodionatamaniuc@gmail.com>
2024-01-11 16:50:54 +0200

    Completed adding Create Event form

---
+++ lib/actions/category.actions.ts
@@ -0,0 +1,11 @@
+"use server"
+
+import { CreateCategoryParams } from "@/types"
+
+export const createCategory = async ({ categoryName }: CreateCategoryParams) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
